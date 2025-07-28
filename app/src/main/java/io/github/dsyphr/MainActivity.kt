47745c6d6f7b4bc9f77452354ac04bce00d4fd2c
AARAV SINGH <aaravsingh412006@gmail.com>
2025-07-28 23:08:06 +0530

    initial UI design

--- app/src/main/java/io/github/dsyphr/MainActivity.kt
+++ app/src/main/java/io/github/dsyphr/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.dsyphr.screens.home.HomeScreen
 import io.github.dsyphr.ui.theme.DsyphrTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DsyphrTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DsyphrTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/io/github/dsyphr/screens/home/homescreen.kt
@@ -0,0 +1,80 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package io.github.dsyphr.screens.home
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import io.github.dsyphr.screens.home.components.ChatSearchBar
+
+
+@Composable
+fun HomeScreen(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Dsyphr")},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Settings,
+                            contentDescription = "Settings"
+                        )
+                    }
+                },
+                navigationIcon = {
+                    // logo
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                onClick = {
+                    // do something
+                }
+            ) {
+                Icon(Icons.Filled.Create, contentDescription = "Create a chat")
+            }
+        },
+
+    ) {innerPadding ->
+
+        Column(modifier = Modifier.padding(top = 64.dp)){
+            ChatSearchBar()
+            //ChatList()
+        }
+
+
+    }
+
+}
