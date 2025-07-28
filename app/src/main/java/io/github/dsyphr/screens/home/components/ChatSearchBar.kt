a4c139f8e8646a6d88ae1c5492a5b384d7d80f18
fine2006 <68106810+fine2006@users.noreply.github.com>
2025-07-29 00:23:34 +0530

    add searchbar dynamic expansion

--- app/src/main/java/io/github/dsyphr/screens/home/components/ChatSearchBar.kt
+++ app/src/main/java/io/github/dsyphr/screens/home/components/ChatSearchBar.kt
@@ -1,6 +1,8 @@
 package io.github.dsyphr.screens.home.components
 
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,21 +20,32 @@ fun ChatSearchBar(modifier: Modifier = Modifier) {
     var searchText by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
     SearchBar(
-        modifier = modifier, query = searchText, active = active, onQueryChange = { it: String ->
-        searchText = it
-    }, onSearch = {
-        active = false
-    }, onActiveChange = {
-        active = it
-    }, placeholder = {
-        Text(text = "Search a contact")
-    }, leadingIcon = {
-        Icon(
-            imageVector = Icons.Filled.Search, contentDescription = "Search a contact"
-        )
-    }, windowInsets = WindowInsets(top = 0.dp)
-
-
+        modifier = Modifier.then(
+            if (active) {
+                Modifier.fillMaxWidth()
+            } else {
+                Modifier.padding(horizontal = 16.dp)
+            }
+        ),
+        query = searchText, active = active,
+        onQueryChange = { it: String ->
+            searchText = it
+        },
+        onSearch = {
+            active = false
+        },
+        onActiveChange = {
+            active = it
+        },
+        placeholder = {
+            Text(text = "Search a contact")
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search, contentDescription = "Search a contact"
+            )
+        },
+        windowInsets = WindowInsets(top = 0.dp),
     ) {}
 }
 
