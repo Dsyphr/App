933f3f9a93748cfc39014677c727a87806605091
AARAV SINGH <aaravsingh412006@gmail.com>
2025-07-28 23:08:06 +0530

    initial UI design

--- app/src/main/java/io/github/dsyphr/ui/theme/Type.kt
+++ app/src/main/java/io/github/dsyphr/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package io.github.dsyphr.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.github.dsyphr.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,8 +16,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -30,5 +31,7 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+
 )
+
+
