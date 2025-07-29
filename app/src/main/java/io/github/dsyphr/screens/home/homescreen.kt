83d5144b527a3296155d9605dbaa35fa5b1a7b23
fine2006 <animeshpathak.2006@gmail.com>
2025-07-29 20:12:01 +0530

    Revert "Merge branch 'main' of https://github.com/dsyphr/app"
    This reverts commit a704993777ab900c891cd8e6f3c004020e3d2861, reversing
    changes made to 029ad4e44aab1a517305f9f04c28b30e31d55c9b.

--- app/src/main/java/io/github/dsyphr/screens/home/homescreen.kt
+++ app/src/main/java/io/github/dsyphr/screens/home/homescreen.kt
@@ -2,76 +2,75 @@
 
 package io.github.dsyphr.screens.home
 
-import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.dsyphr.screens.home.components.ChatSearchBar
+import io.github.dsyphr.screens.home.components.ContactListItem
 
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = {Text("Dsyphr")},
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+            Column(modifier = Modifier.padding(bottom = 10.dp)) {
+                TopAppBar(
+                    title = {
+                    Text(
+                        "Dsyphr", style = TextStyle(
+                            brush = Brush.horizontalGradient(
+                                colors = listOf(
+                                    Color(0xFFF45DFF), Color(0xFF7907FF),
+                                )
+                            )
+                        ), fontSize = 30.sp, fontWeight = FontWeight.ExtraBold
+                    )
+                }, colors = TopAppBarDefaults.topAppBarColors(
                 ),
 
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Settings,
-                            contentDescription = "Settings"
-                        )
-                    }
-                },
-                navigationIcon = {
-                    // logo
-                }
-            )
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Settings, contentDescription = "Settings"
+                            )
+                        }
+                    }, navigationIcon = {
+                        // logo
+                    })
+
+                ChatSearchBar()
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
-                containerColor = MaterialTheme.colorScheme.primary,
-                onClick = {
+                containerColor = MaterialTheme.colorScheme.primary, onClick = {
                     // do something
-                }
-            ) {
+                }) {
                 Icon(Icons.Filled.Create, contentDescription = "Create a chat")
             }
         },
 
-    ) {innerPadding ->
+        ) { innerPadding ->
 
-        Column(modifier = Modifier.padding(top = 64.dp)){
-            ChatSearchBar()
-            //ChatList()
+        Column(modifier = Modifier.padding(innerPadding)) {
+            LazyColumn {
+                items(50) {
+                    ContactListItem()
+                    //HorizontalDivider()
+                }
+            }
         }
 
 
