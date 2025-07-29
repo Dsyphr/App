4051d61b0d09e91f9fdaa8ee3b4537cf83f9a21d
fine2006 <68106810+fine2006@users.noreply.github.com>
2025-07-29 22:54:30 +0530

    Add RECEIVED and READ_RECEIVED states to enum MessageStatus

--- app/src/main/java/io/github/dsyphr/enums/Enums.kt
+++ app/src/main/java/io/github/dsyphr/enums/Enums.kt
@@ -1,4 +1,4 @@
 package io.github.dsyphr.enums
 
-enum class ReadStatus { SENT, DELIVERED, READ }
+enum class ReadStatus { SENT, DELIVERED, READ , RECEIVED, RECEIVED_READ }
 enum class MessageParty { SELF, OTHER }
--- app/src/main/java/io/github/dsyphr/screens/home/components/ContactListItem.kt
+++ app/src/main/java/io/github/dsyphr/screens/home/components/ContactListItem.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.DoneAll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -25,30 +27,42 @@ fun ContactListItem(
     name: String = "Default Contact",
     lastMessageTime: String = "12:35 pm",
     lastMessage: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum gravida feugiat. In tincidunt sem porttitor convallis eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    readStatus: Enum<ReadStatus> = ReadStatus.READ
+    readStatus: Enum<ReadStatus> = ReadStatus.RECEIVED
 ) {
-    ListItem(
-        modifier = modifier.padding(vertical = 2.dp),
-        headlineContent = { Text(name) },
-        supportingContent = {
-            Text(
-                softWrap = true, maxLines = 1, overflow = TextOverflow.Ellipsis, text = lastMessage
-            )
-        },
-        leadingContent = {
-            Icon(
-                Icons.Filled.AccountCircle, contentDescription = null, modifier = Modifier.size(60.dp)
-            )
-        },
-        trailingContent = {
-            Column(horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.SpaceEvenly) {
-                Text(lastMessageTime)
-                Icon(
-                    if (readStatus == ReadStatus.SENT) Icons.Filled.Done else Icons.Filled.DoneAll,
-                    contentDescription = null,
-                    modifier = Modifier.size(18.dp),
-                    tint = if (readStatus == ReadStatus.READ) Color.Cyan else Color.Gray
-                )
+    ListItem(modifier = modifier.padding(vertical = 2.dp), headlineContent = { Text(name) }, supportingContent = {
+        Text(
+            softWrap = true, maxLines = 1, overflow = TextOverflow.Ellipsis, text = lastMessage
+        )
+    }, leadingContent = {
+        Icon(
+            Icons.Filled.AccountCircle, contentDescription = null, modifier = Modifier.size(60.dp)
+        )
+    }, trailingContent = {
+        Column(horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.SpaceEvenly) {
+            Text(lastMessageTime)
+            when (readStatus) {
+                ReadStatus.RECEIVED -> {
+                    val primaryColor = MaterialTheme.colorScheme.primary
+                    Text(
+                        "1", modifier = Modifier.padding(10.dp).drawBehind {
+                            drawCircle(
+                                radius = this.size.maxDimension / 1.5f, color = primaryColor
+                            )
+                        }, color = MaterialTheme.colorScheme.onPrimary )
+                }
+
+                ReadStatus.RECEIVED_READ -> {
+                }
+
+                else -> {
+                    Icon(
+                        if (readStatus == ReadStatus.SENT) Icons.Filled.Done else Icons.Filled.DoneAll,
+                        contentDescription = null,
+                        modifier = Modifier.size(18.dp),
+                        tint = if (readStatus == ReadStatus.READ) Color.Cyan else Color.Gray
+                    )
+                }
             }
-        })
+        }
+    })
 }
