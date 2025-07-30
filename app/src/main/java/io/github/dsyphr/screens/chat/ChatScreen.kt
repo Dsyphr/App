cf74373b4ba24a0408343d145b6ad58993e5c8b1
fine2006 <68106810+fine2006@users.noreply.github.com>
2025-07-30 23:59:43 +0530

    Fix wrong chatnames showing up

--- app/src/main/java/io/github/dsyphr/screens/chat/ChatScreen.kt
+++ app/src/main/java/io/github/dsyphr/screens/chat/ChatScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Send
@@ -13,28 +14,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.dsyphr.dataClasses.MessageItem
 import io.github.dsyphr.dataClasses.User
+import io.github.dsyphr.dataClasses.messageItems
 import io.github.dsyphr.enums.MessageParty
 import io.github.dsyphr.screens.chat.components.MessageCard
 
 
-val joe = User("John doe", 1, )
-val jake = User("jake ", id = 2)
-
-val msg1 = MessageItem("Hey, how are you doing", joe, jake)
-val msg2 = MessageItem("Hey, how are you doing", jake, joe)
-val messageItems = mutableListOf<MessageItem>(msg1, msg2)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ChatScreen(modifier: Modifier = Modifier,currentUser: User) {
+fun ChatScreen(modifier: Modifier = Modifier,secondUser: User) {
     Scaffold(topBar = {
 
         TopAppBar(
-            title = { Text(text = currentUser.username) },
+            title = { Text(text = secondUser.username) },
             navigationIcon = {
                 IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Person, contentDescription = "person logo"
-                    )
+                    secondUser.profileImg
                 }
             },
             actions = {
@@ -57,7 +51,7 @@ fun ChatScreen(modifier: Modifier = Modifier,currentUser: User) {
             modifier = Modifier.fillMaxSize(), reverseLayout = true, contentPadding = innerPadding// 5
         ) {
             items(messageItems) { messageItem ->
-                MessageCard(messageItem, currentUser)
+                MessageCard(messageItem, secondUser)
             }
         }
 
@@ -87,7 +81,7 @@ fun BasicChatInput(modifier: Modifier = Modifier) {
             shape = MaterialTheme.shapes.extraLarge,
         ) {
             Icon(
-                Icons.Default.Send, contentDescription = "send", modifier = modifier
+                Icons.AutoMirrored.Filled.Send, contentDescription = "send", modifier = modifier
             )
         }
     }
